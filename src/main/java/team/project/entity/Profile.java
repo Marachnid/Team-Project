87d5613acb835bc2ee08aca811cc0a00ddd65eec
Marachnid <mcherry2@madisoncollege.edu>
2025-04-01 20:51:27 -0500

    Created a POJO used by CalculateMetrics to flexible store data
    Crated a DB clean script for profiles DB table

---
+++ src/main/java/team/project/entity/Profile.java
@@ -0,0 +1,55 @@
+package team.project.entity;
+
+public class Profile {
+
+    private int age;
+    private String sex;
+    private int height;
+    private int weight;
+
+
+    public Profile() {}
+
+
+    public Profile(int age, String sex, int height, int weight) {
+        this.age = age;
+        this.sex = sex;
+        this.height = height;
+        this.weight = weight;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+
+}
