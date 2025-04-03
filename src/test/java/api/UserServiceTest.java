5d7d49971fa46aca95430122006561650d750900
Briceann <btaneh@madisoncollege.edu>
2025-04-03 12:02:21 -0500

    added tests for the methods in UserService class and also added a method in UserService class to clear all existing data before each test is executed

--- src/test/java/api/UserServiceTest.java
+++ src/test/java/api/UserServiceTest.java
@@ -15,18 +15,78 @@ public class UserServiceTest {
     @BeforeEach
     void setUp() {
         userService = new UserService();
+        userService.resetUsers();
     }
 
-    //TODO this is the start of a testing class UserService - use this first test as an example to continue testing other methods in UserService class until we have full coverage!
     @Test
     void testAddUser() {
         Profile user = new Profile();
-        user.setId(1);
         Response response = userService.addUser(user);
 
         assertEquals(201, response.getStatus());
         Profile createdUser = (Profile) response.getEntity();
-        assertEquals(1, createdUser.getId());
+        assertNotNull(createdUser.getId());
         assertEquals(response.getStatus(), Response.Status.CREATED.getStatusCode());
+    }
+
+    @Test
+    void testGetUserById() {
+        Profile user = new Profile();
+        Response addResponse = userService.addUser(user);
+        Profile createdUser = (Profile) addResponse.getEntity();
+
+        Response response = userService.getUserById(createdUser.getId());
+        assertEquals(200, response.getStatus());
+
+        Profile fetchedUser = (Profile) response.getEntity();
+        assertEquals(createdUser.getId(), fetchedUser.getId());
+        assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
+
+    }
+
+    @Test
+    void testGetAllUsers() {
+        Profile user1 = new Profile();
+        Profile user2 = new Profile();
+        userService.addUser(user1);
+        userService.addUser(user2);
+
+        Response response = userService.getAllUsers();
+        assertEquals(200, response.getStatus());
+
+        List<Profile> users = (List<Profile>) response.getEntity();
+        assertEquals(2, users.size());
+    }
+
+    @Test
+    void testUpdateUser() {
+        Profile user = new Profile();
+        Response addResponse = userService.addUser(user);
+        Profile createdUser = (Profile) addResponse.getEntity();
+
+        Profile updatedUser = new Profile();
+        updatedUser.setId(createdUser.getId());
+
+        Response response = userService.updateUser(createdUser.getId(), updatedUser);
+        assertEquals(200, response.getStatus());
+
+        Profile returnedUser = (Profile) response.getEntity();
+        assertEquals(createdUser.getId(), returnedUser.getId());
+
+    }
+
+    @Test
+    void testDeleteUser() {
+        Profile user = new Profile();
+        Response addResponse = userService.addUser(user);
+        Profile createdUser = (Profile) addResponse.getEntity();
+
+        Response response = userService.deletePerson(createdUser.getId());
+        assertEquals(204, response.getStatus());
+
+        Response getResponse = userService.getUserById(createdUser.getId());
+        assertEquals(getResponse.getStatus(), Response.Status.NOT_FOUND.getStatusCode());
+
+
     }
 }
