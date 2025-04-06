6c5758ac0b2a8167dcf1e9184710669483d77fa7
Marachnid <mcherry2@madisoncollege.edu>
2025-04-05 20:46:13 -0500

    Built out testing methods and removed outdated sql script

--- src/test/java/test/TestDAOTest.java
+++ src/test/java/test/TestDAOTest.java
@@ -3,67 +3,91 @@ package test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import utilities.Database;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+
+/**
+ * Main testing class for Profile DAO operations
+ */
 class TestDAOTest {
 
     TestDAO dao;
     Database database;
+    ProfileEntity profile;
+    ProfileEntity alteredProfile;
+    List<ProfileEntity> profiles;
 
 
+    /**Sets up testing environment*/
     @BeforeEach
     void setUp() {
         dao = new TestDAO();
-
+        profiles = new ArrayList<>();
         database = Database.getInstance();
-        database.runSQL("table_clean.sql");
-
+        database.runSQL("profiles_clean.sql");
     }
 
+
+    /**Tests getting a profile by its ID*/
     @Test
-    void getById() {
-
-        ProfileEntity profile;
-
+    void getByIdSuccess() {
         profile = dao.getById(1);
-        System.out.println(profile.getId());
-        System.out.println(profile.getAge());
-        System.out.println(profile.getWeight());
-        System.out.println(profile.getHeight());
-        System.out.println(profile.getSex());
+        assertNotNull(profile);
+        assertEquals(20, profile.getAge());
+        assertEquals(150, profile.getWeight());
     }
 
 
-
+    /**Tests retrieving all profiles*/
     @Test
-    void getAll() {
-
-        List<ProfileEntity> profiles = new ArrayList<>();
-
+    void getAllSuccess() {
         profiles = dao.getAllProfiles();
-
-
-        for (ProfileEntity profile : profiles) {
-            System.out.println(profile.getId());
-            System.out.println(profile.getAge());
-            System.out.println(profile.getWeight());
-            System.out.println(profile.getHeight());
-            System.out.println(profile.getSex());
-        }
+        assertEquals(6, profiles.size());
     }
 
 
+    /**Tests adding a profile*/
     @Test
-    void deleteProfile() {
+    void addProfileSuccess() {
+        profile = new ProfileEntity(20, 70, 185, "male", 1);
+        dao.insertProfile(profile);
 
-        ProfileEntity profileToDelete = dao.getById(1);
-        dao.deleteProfile(profileToDelete);
+        alteredProfile = dao.getById(profile.getId());
+        assertNotNull(alteredProfile);
+        assertEquals(20, alteredProfile.getAge());
+        assertEquals(185, alteredProfile.getWeight());
+    }
+
+
+    /**Tests updating a profile*/
+    @Test
+    void updateProfileSuccess() {
+        profile = dao.getById(1);
+        profile.setAge(45);
+        profile.setHeight(75);
+        profile.setWeight(200);
+        profile.setActivity(2);
+        dao.updateProfile(profile);
+
+        alteredProfile = dao.getById(1);
+        assertNotNull(alteredProfile);
+        assertEquals(45, alteredProfile.getAge());
+        assertEquals(75, alteredProfile.getHeight());
+        assertEquals(200, alteredProfile.getWeight());
+        assertEquals(2, alteredProfile.getActivity());
+    }
+
+
+    /**Tests deleting a profile*/
+    @Test
+    void deleteProfileSuccess() {
+        profile = dao.getById(1);
+        dao.deleteProfile(profile);
 
         assertNull(dao.getById(1));
+        assertEquals(5, dao.getAllProfiles().size());
     }
 
 
